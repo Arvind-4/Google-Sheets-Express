5c554c7ca7c16795b1260a0b0eeb74a356ac2b64
Arvind-4 <lordarvindthegamer@gmail.com>
2023-06-07 15:52:28 +0530

    Updated: for env variables

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,6 +9,17 @@ export default {
   logs: {
     level: process.env.LOG_LEVEL || 'silly',
   },
+  google: {
+    projectID: process.env.GOOGLE_PROJECT_ID,
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    serviceType: process.env.GOOGLE_SERVICE_TYPE,
+    tokenUri: process.env.GOOGLE_TOKEN_URI,
+    redirectUri: process.env.GOOGLE_REDIRECT_URI,
+    privateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    email: process.env.GOOGLE_CLIENT_EMAIL,
+    spreadsheetId: process.env.GOOGLE_SPREAD_SHEET_ID,
+    scopes: [process.env.GOOGLE_SCOPES],
+  },
   api: {
     prefix: '/api',
   },
